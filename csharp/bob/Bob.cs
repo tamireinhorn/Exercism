cc85f5e6115d9c9f7128f125b0795ab51ecd8ca8
tamireinhorn <tamtamireinhorn@gmail.com>
2021-12-22 15:33:33 +0200

    Resolve exercise with mentoring suggestions
    Finally!

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+
+public static class Bob
+{
+    public static string Response(string statement)
+    {
+       
+        statement = statement.Trim();
+        bool isEmpty = string.IsNullOrEmpty(statement);
+        if(isEmpty){
+            return "Fine. Be that way!";
+        }
+        bool isAlpha = statement.Any(s => char.IsLetter(s));
+        bool isYell = statement == statement.ToUpper() && isAlpha;
+        bool isQuestion = false;
+        isQuestion = statement.EndsWith('?');
+        if (isQuestion && isYell){
+            return "Calm down, I know what I'm doing!";
+            }
+        else if (isQuestion && !isYell){
+            return "Sure.";
+        }
+        else if(!isQuestion && isYell){
+            return "Whoa, chill out!";
+            
+        }
+        
+        else {
+            return "Whatever.";
+        }
+        
+
+        
+    }
+}
