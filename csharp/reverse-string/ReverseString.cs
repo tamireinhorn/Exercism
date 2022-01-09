c32bdfd64ba77530ad0a2730996a83c338af6245
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-10 01:18:38 +0200

    Reverse String.cs

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var x = input.ToCharArray();
+        int Size = x.Length;
+        for(int i = 0;  i < Size; i++){
+            x[Size - 1 - i] = input[i];
+        }
+        return new string(x);
+
+    }
+}
