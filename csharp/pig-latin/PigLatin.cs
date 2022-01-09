1349c68642264a77810f6cfa0d87f42a96b704b9
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-09 02:02:40 +0200

    Refactoring exercises

--- csharp/pig-latin/PigLatin.cs
+++ csharp/pig-latin/PigLatin.cs
@@ -17,25 +17,30 @@ public static class PigLatin
         Regex VowelString = new Regex($"\\A({VowelSounds})");
         string Beg = word.Substring(0,2);
         bool StartsWithVowel = VowelString.Match(Beg).Success;
-        if (StartsWithVowel){ // Rule 1, matching for vowel sounds. 
+        if (StartsWithVowel)
+        { // Rule 1, matching for vowel sounds. 
             return word + suffix;
         }
-        else if (new Regex("^[a-z]y$").IsMatch(word)){ // Rule 4, matching for two letter words with y at the end. 
+        else if (new Regex("^[a-z]y$").IsMatch(word))
+        { // Rule 4, matching for two letter words with y at the end. 
             return String.Join("",word.Reverse().ToArray()) + suffix;
         }
         else{ //Starts with a consonant cluster.
             var NextVowel = new Regex($"({VowelSounds}|(?<=[a-z])y)").Match(word);
             var HasQu = new Regex("qu").Match(word);
-            if (!NextVowel.Success){ //Words with only consonants should be treated like this.
+            if (!NextVowel.Success)
+            { //Words with only consonants should be treated like this.
                 return word + suffix;
-                }
-            if (HasQu.Success){
-                return word.Substring(HasQu.Index + HasQu.Length) + word.Substring(0, HasQu.Index + HasQu.Length) + suffix;;
+            }
+            if (HasQu.Success)
+            {
+                return word.Substring(HasQu.Index + HasQu.Length) + word.Substring(0, HasQu.Index + HasQu.Length) + suffix;
             }
             return word.Substring(NextVowel.Index) + word.Substring(0, NextVowel.Index) + suffix;
         }
     }
-    public static string Translate(string word){
+    public static string Translate(string word)
+    {
         return string.Join(" ", word.Split().Select(x => x.TranslateWord()));
     }
 }
