73d909ccd9da361aa47d03f44045dad449eaa254
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-13 23:02:55 +0200

    Update allergies.py

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,6 +1,6 @@
 ALLERGIES_SCORE = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8,
 'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
-SCORE_ALLERGIES = {score: item for item, score in sorted(ALLERGIES_SCORE.items(), key = lambda item: item[1], reverse= True)}
+SCORE_ALLERGIES = {score: item for item, score in sorted(ALLERGIES_SCORE.items(), key = lambda item: item[1])}
 
 from copy import copy
 
@@ -17,36 +17,11 @@ class Allergies:
 
 
     def list_of_allergies(self):
-        score = self.score 
+        score = format(self.score, 'b')[::-1]
         allergy_list = []
-        score_allergies = copy(SCORE_ALLERGIES)
-        a_score = score_allergies.get(score, -1)
-        #If the score is EXACTLY one allergen, it's done.
-        if a_score != -1:
-            allergy_list.append(a_score)
-            return allergy_list
-        #If the score is odd, then 'eggs' are an allergen for sure.
-        if score % 2 != 0:
-            allergy_list.append(score_allergies[1])
-            del score_allergies[1]
-            score -= 1
-        #Now, we loop over the biggest items and go adding the allergies.
-        for allergen_score, item in score_allergies.items():
-            diff = score - allergen_score
-            #Our dict is incomplete.
-            #Therefore, if we have still 256 of allergic score and nothing on the dict
-            #We KNOW that if 2 is in 256, the remanining 254 won't fit!
-            # So if we are on the last item (which has the smallest value)
-            # And score - allergen_score will have SOMETHING left but will be too small for the next allergen: STOP.
-            
-            if allergen_score == min(score_allergies) and 0 < diff < max(score_allergies) * 2:
-                break
-            if diff >= 0:
-                score -= allergen_score
-                allergy_list.append(item)
-            #We stop when we run out of 'allergic score'  
-            if score <= 0:
-                break
+        for character, allergen in zip(score, SCORE_ALLERGIES.items()):
+            if character == '1':
+                allergy_list.append(allergen[1])
         return allergy_list
 
 
