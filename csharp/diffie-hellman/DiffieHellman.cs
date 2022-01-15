28fd6a53192ea9dc9b1c023bfbb00baa646bc7a5
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-15 04:42:29 +0200

    Diffie-Helman.cs
    Solved

---
+++ csharp/diffie-hellman/DiffieHellman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+public static class DiffieHellman
+{
+    public static BigInteger PrivateKey(BigInteger primeP) 
+    {
+        return new Random().NextInt64(2, (long)primeP-1);
+    }
+ 
+    public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey) 
+    {
+        return (BigInteger)Math.Pow((double)primeG, (double)privateKey) % primeP;
+    }
+
+    public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey) 
+    {
+        return (BigInteger)Math.Pow((double)publicKey, (double)privateKey) % primeP;
+    }
+}
