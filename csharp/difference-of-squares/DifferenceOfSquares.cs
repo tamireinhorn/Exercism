d517731da7c64c8feefc2ef5d7908e7183e352d4
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-15 08:07:08 +0200

    Difference of Squares.cs

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+public static class DifferenceOfSquares
+{ 
+    public static int CalculateSquareOfSum(int max)
+    {
+        return (int)Math.Pow((double)Enumerable.Range(1, max).Select(x => x).Sum(),2.0);
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+       return Enumerable.Range(1, max).Select(n => n * n).Sum();
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
