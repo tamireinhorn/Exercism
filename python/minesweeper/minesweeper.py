66bb4e24ae3602f420220c0ed23a455a81215afd
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-25 18:45:11 +0200

    Update minesweeper.py
    For some reason, my local code was not the correct one.

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -14,28 +14,42 @@ def nearby_cells(number_of_rows, number_of_columns, index: tuple):
     right =  [(row_index, column_index + 1)]
     left =   [(row_index, column_index -1)]
     above_left = [(row_index-1, column_index-1)]
-
     above_right =  [(row_index-1, column_index+1)]
     below_left =  [(row_index+1, column_index-1)]
     below_right =  [(row_index+1, column_index+1)]
-    if is_upper_vertical_edge and is_left_horizontal_edge:
-        answer = below + right + below_right
-    elif is_upper_vertical_edge and is_right_horizontal_edge:
-        answer = below + left + below_left
-    elif is_lower_vertical_edge and is_left_horizontal_edge:
-        answer = above + right + above_right
-    elif is_lower_vertical_edge and is_right_horizontal_edge:
-        answer = above + left + above_left
-    elif is_upper_vertical_edge:
-        answer = below + right + below_right + left + below_left
-    elif is_lower_vertical_edge:
-        answer = above + right + left + above_left + above_right
-    elif is_left_horizontal_edge:
-        answer = above + below + right + above_right + below_right
-    elif is_right_horizontal_edge:
-        answer = above + below + left + above_left + below_left
+    if number_of_rows == 1:
+        if is_left_horizontal_edge:
+            answer = right
+        elif is_right_horizontal_edge:
+            answer = left
+        else:
+            answer = left + right
+    elif number_of_columns == 1:
+        if is_upper_vertical_edge:
+            answer = below
+        elif is_lower_vertical_edge:
+            answer = above
+        else:
+            answer = above + below
     else:
-        answer = above + above_right + above_left + below_left + below_right + below
+        if is_upper_vertical_edge and is_left_horizontal_edge:
+            answer = below + right + below_right
+        elif is_upper_vertical_edge and is_right_horizontal_edge:
+            answer = below + left + below_left
+        elif is_lower_vertical_edge and is_left_horizontal_edge:
+            answer = above + right + above_right
+        elif is_lower_vertical_edge and is_right_horizontal_edge:
+            answer = above + left + above_left
+        elif is_upper_vertical_edge:
+            answer = below + right + below_right + left + below_left
+        elif is_lower_vertical_edge:
+            answer = above + right + left + above_left + above_right
+        elif is_left_horizontal_edge:
+            answer = above + below + right + above_right + below_right
+        elif is_right_horizontal_edge:
+            answer = above + below + left + above_left + below_left
+        else:
+            answer = above + above_right + above_left + below_left + below_right + below + left + right
     return answer
 
 
@@ -44,23 +58,24 @@ def annotate(minefield):
         return minefield
     number_of_rows = len(minefield)
     number_of_columns = len(minefield[0])
-    annotation = [[0] * number_of_columns] * number_of_rows
+    if len(set(list(map(len, minefield)))) > 1:
+        raise ValueError("The board is invalid with current input.")
+    annotation = [ [ 0 for i in range(number_of_columns) ] for i in range(number_of_rows) ]
     for row_number, row in enumerate(minefield):
         for column_number, element in enumerate(row):
             if element == '*': # Is mine.
-                print(row_number, column_number)
+                annotation[row_number][column_number] = '*'
                 #Now we add +1 to the corresponding guys, given by the function
-                #print(row_number, column_number)
                 next_squares = nearby_cells(number_of_rows, number_of_columns, (row_number, column_number))
                 for squares in next_squares:
                     row_index = squares[0]
                     column_index = squares[1]
-                    
                     if minefield[row_index][column_index] != '*':
                         annotation[row_index][column_index] += 1
-                    else:
-                        annotation[row_index][column_index] = '*'
+            elif element != ' ':
+                raise ValueError("The board is invalid with current input.")
     for row_number, row in enumerate(annotation):
         annotation[row_number] = ''.join(list(map(str, row))).replace('0', ' ')
     return annotation
+
 
