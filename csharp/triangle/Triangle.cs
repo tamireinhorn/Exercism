b1eefa5127e1f4338ef8f434bdfc5dc470d18d5a
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-26 13:26:43 +0200

    Update Triangle.cs
    Changed some methods to be expression level just to clean up my code.

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -10,35 +10,13 @@ public static class Triangle
         bool inequality = 2 * sides.Max() < sides.Sum();
         return allSidesPos & inequality;
     }
-    public static bool IsScalene(double side1, double side2, double side3)
-    {
-         if (Triangle.IsValid(side1, side2, side3)){
-            return (side1 != side2) & (side2 != side3) & (side1 != side3);
-        }
-        else{
-            return false;
-        }
-        
-    }
- 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
-    {
-        if (Triangle.IsValid(side1, side2, side3)){
-            return (side1 == side2) | (side2 == side3) | (side1 == side3);
-        }
-        else{
-            return false;
-        }
-    }
+    public static bool IsScalene(double side1, double side2, double side3) => IsValid(side1, side2, side3) & ((side1 != side2) & (side2 != side3) & (side1 != side3));
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
-    {
-        if (Triangle.IsValid(side1, side2, side3)){
-            double side = side1;
-            return (side == side2) & (side== side3);
-        }
-        else{
-            return false;
-        }
-    }  
+ 
+    public static bool IsIsosceles(double side1, double side2, double side3) => IsValid(side1, side2, side3) & ((side1 == side2) | (side2 == side3) | (side1 == side3));
+
+
+    public static bool IsEquilateral(double side1, double side2, double side3) => IsValid(side1, side2, side3) & ((side1 == side3) & (side2 == side3));
+
+    
 }
