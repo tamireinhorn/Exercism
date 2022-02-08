a05aad3b26604610fd114e3a1f47a463472ab1a1
tamireinhorn <tamtamireinhorn@gmail.com>
2022-02-08 18:18:05 +0200

    Update change.py
    Recursive solution, still not all encompassing, still too slow.

--- python/change/change.py
+++ python/change/change.py
@@ -1,25 +1,32 @@
-from itertools import chain
-from functools import partial
-import numpy as np 
-
-
-def comparison(x, y, flag_for_less = 1):
-    if flag_for_less == 1:
-        return x <= y
+from copy import copy
+def return_smallest_list(list1, list2):
+    if len(list1) < len(list2):
+        return list1
     else:
-        return y <= x
+        return list2
+def coin_recursion(coins: list[int], target:int, current_change: list[int]):
+    # Base case for recursion: target is directly a value in coins
+    if target in coins:
+        current_change.append(target)
+        return current_change
+    # If it's not, we gotta recurse.
+    # Generate a list of viable coins.
+    possible_coins = [coin for coin in coins if coin <= target]
+    counter = [0 for i in range(target*10)]
+    print(2)
+    for coin in possible_coins:
+        new_change = copy(current_change)
+        new_change.append(coin)
+        eval = coin_recursion(coins, target - coin, new_change)
+        counter = return_smallest_list(eval, counter)
+    return counter
 
-
-# def recursion(coins, target, item):
-#     if item == 0:
-        
 
 def find_fewest_coins(coins: list[int], target: int):
-    viable_coins = list(filter(partial(comparison, y = target), coins))
-    viable_median = np.median(viable_coins)
-    viable_coins = list(filter(partial(comparison, y = viable_median, flag_for_less = 0), viable_coins))
-    target_list = [target - coin for coin in viable_coins]
-    print(target_list)
+    current_change = []
+    if target < 0:
+        raise ValueError("target can't be negative")
+    return coin_recursion(coins, target, current_change)
     
 
 ##Pega todas as moedas abaixo do valor escolhido.
