ee891f3a587736e6cc26bb3c4125ca43cde60f4b
tamireinhorn <tamtamireinhorn@gmail.com>
2022-02-11 15:50:50 +0200

    Solved change with very good icommunity sol

--- python/change/change.py
+++ python/change/change.py
@@ -1,35 +1,20 @@
-from copy import copy
-def return_smallest_list(list1, list2):
-    if len(list1) < len(list2):
-        return list1
-    else:
-        return list2
-def coin_recursion(coins: list[int], target:int, current_change: list[int]):
-    # Base case for recursion: target is directly a value in coins
-    if target in coins:
-        current_change.append(target)
-        return current_change
-    # If it's not, we gotta recurse.
-    # Generate a list of viable coins.
-    possible_coins = [coin for coin in coins if coin <= target]
-    counter = [0 for i in range(target*10)]
-    print(2)
-    for coin in possible_coins:
-        new_change = copy(current_change)
-        new_change.append(coin)
-        eval = coin_recursion(coins, target - coin, new_change)
-        counter = return_smallest_list(eval, counter)
-    return counter
-
-
-def find_fewest_coins(coins: list[int], target: int):
-    current_change = []
+from itertools import combinations_with_replacement
+## This solution is BRILLIANT but is based on a a community solution from RNeilsen, avaliable here: https://exercism.org/tracks/python/exercises/change/solutions/RNeilsen
+def find_fewest_coins(coins, target):
     if target < 0:
         raise ValueError("target can't be negative")
-    return coin_recursion(coins, target, current_change)
-    
-
-##Pega todas as moedas abaixo do valor escolhido.
-## Calcula a mediana delas
-## Seleciona, dentre elas, as acima da mediana. 
-## Faça valor - estas moedas, gerando uma nova lista. Se o valor for 0 ou igual a uma moeda, já era.
+    # The core idea is that the biggest possible solution is just target // the smallest coin.
+    max_coins = target // min(coins)
+    # Also, the smallest possible solution is target // biggest coin.
+    min_coins = target // max(coins)
+    # To range over this, we just sum +1 
+    for i in range(min_coins, max_coins + 1):
+        # This is the bomb. This simply creates all possible combinations of size i from a list, with replacement (as the coins are infinite).
+        combos = combinations_with_replacement(coins, i)
+        # Now, you just loop over all possible combinations
+        for combo in combos:
+            # If the combo equals the target, we're done, just return it sorted as that's what the answer expects. Done.
+            if sum(combo) == target:
+                return sorted(combo)
+    # If we never stop, the target is impossible.
+    raise ValueError("can't make target with given coins")
