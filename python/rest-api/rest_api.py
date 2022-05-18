e2f58a4c8efc2722c19caa287d11206b039b6b0c
tamireinhorn <tamtamireinhorn@gmail.com>
2022-05-18 20:38:49 +0300

    Updated Rest Api.py with mentor suggestions

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -1,14 +1,25 @@
+from copy import copy
 import json 
-
+from operator import itemgetter
 
 def _clean_url(url: str) -> str :
     return url.removeprefix('/')
 
+def get_keys(dict: dict):
+    return list(map(itemgetter(0), dict.items()))
+
 
 class RestAPI:
     def __init__(self, database: dict =None):
-        self.database = database 
+        
+        for entry in database['users']:
+            possible_users = set(get_keys(entry.get('owes')) + get_keys(entry.get('owed_by'))) # This gets all unique users in that entry!
+            for user in possible_users:
+                user_amount = entry.get('owes').get(user, 0) - entry.get('owed_by').get(user, 0) # We aggregate the debt of that user in the entry in one dict.
+                entry['owes'][user] = user_amount # We redo the owes dictionary
+                del entry['owed_by'] # We remove the unnecessary dictionary.
         self.valid_gets = ['users']
+        self.database = database 
 
     def get(self, url: str, payload: str = None):
         clean_url = _clean_url(url)
@@ -37,48 +48,38 @@ class RestAPI:
     def __add_user(self, payload: str) -> str:
         users_list = self.database['users']
         new_user = payload['user']
-        new_user_entry = {'name': new_user , 'owes': {}, 'owed_by': {}, 'balance': 0.0}
+        new_user_entry = {'name': new_user , 'owes': {}, 'balance': 0.0}
+        return_new_user_entry = copy(new_user_entry)
+        return_new_user_entry['owed_by'] = {}
         users_list.append(new_user_entry)
-        return json.dumps(new_user_entry)
+        return json.dumps(return_new_user_entry)
     
     def __add_iou(self, payload: str) -> str:
         # For the lender, we have to do:
         lender = payload['lender']
         borrower = payload['borrower']
         amount = payload['amount']
-        lender_entry = self.__modify_user(lender, borrower, amount, True)
-        borrower_entry = self.__modify_user(lender, borrower, amount, False)
-        return json.dumps({'users': sorted([lender_entry, borrower_entry], key = lambda x: x['name'])})
+        lender_entry = self.__modify_user(lender, borrower, -amount)
+        borrower_entry = self.__modify_user(borrower, lender, amount)
+        
+        return json.dumps({'users': sorted([lender_entry, borrower_entry], key = lambda x: x['name'])}) # The expected return is ordered by the name of the users in the IOU.   
 
-    def __modify_user(self, lender: str, borrower: str, amount: float, is_lender: bool) -> str:
-        if is_lender:
-            user = lender 
-            other_guy = borrower
-            previous_debt_reference = 'owes' # We wish to verify if the lender user had BORROWED money from the current borrower.
-            current_debt_reference = 'owed_by'
-        else:
-            user = borrower 
-            other_guy = lender 
-            previous_debt_reference = 'owed_by' # We verify if the current lender had previously lonaed the borrower money.
-            current_debt_reference = 'owes'
+    def parse_database_return(self, user: str) -> dict: 
+        # This method exists because the output expects two dicts, one of owes and other of owed_by
+        copy_user_entry = copy(self.__get_user(user))
+        copy_user_entry['owed_by'] = {user: -amount for user, amount in copy_user_entry['owes'].items() if amount < 0} # Negative debt -> user is OWED BY, goes to new dict for output.
+        copy_user_entry['owes'] = {user: amount for user, amount in copy_user_entry['owes'].items() if amount > 0} # Positive debt in owes -> stays
+        return copy_user_entry 
+
+
+    def __modify_user(self, lender: str, borrower: str, amount: float) -> dict:
+        # This is a remodeling of the method I did, but this time assuming debt is a single dictionary.
+        user = lender 
+        other_guy = borrower 
         user_data = self.__get_user(user)
-        if is_lender:
-            user_data['balance'] += amount # A new loan increases the lender's balance.
-        else:
-            user_data['balance'] -= amount # A new loan decreases the borrower's balance. 
-        if other_guy in user_data[previous_debt_reference]: # We want to verify if there was a previous debt where the roles were switched.
-            previous_debt = user_data[previous_debt_reference][other_guy]
-            previous_debt -= amount # Reduce the debt by the new loan
-            if previous_debt > 0: # If despite the new loan, there is still some debt left.
-                user_data[previous_debt_reference][other_guy] = previous_debt # You update the previous debt.
-                return user_data
-            else: 
-                del user_data[previous_debt_reference][other_guy] # The previous debt is erased
-                amount = -1 * previous_debt # The difference between the old debt and the new loan
-        current_debt = user_data[current_debt_reference].get(other_guy, 0) # If there was the same type of debt before, we have to increase it!
-        total = amount + current_debt
-        if total <= 0:
-            return user_data
-        else:
-            user_data[current_debt_reference][other_guy] = total 
-            return user_data
+        user_data['balance'] -= amount # A new loan will always decrease your balance, either decreasing borrower by amount or decreasing lender by -amount (which is a net increase)
+        current_debt = user_data['owes'].get(other_guy, 0) # The current debt is how much the user is owed by other guy
+        total = current_debt + amount # We add the amount, regardless
+        user_data['owes'][other_guy] = total # We update the dictionary
+        parsed_user_data = self.parse_database_return(user) # Parse this due to the expected output.
+        return parsed_user_data
