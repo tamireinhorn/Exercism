3a3d5744e88ab4457df4e6ac20d772d8c50c43da
tamireinhorn <tamtamireinhorn@gmail.com>
2022-05-19 00:25:17 +0300

    Update markdown.py

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,32 +1,32 @@
 import re
 
 def header_parsing(line):
-    m = re.match('#{1,6} ', line) # A header is composed of 1 to 6 #s and a space.
-    if m:
-        size = m.end() -1 # We remove the space's position.
+    header_match = re.match('#{1,6} ', line) # A header is composed of 1 to 6 #s and a space.
+    if header_match:
+        size = header_match.end() -1 # We remove the space's position.
         line = f'<h{size}>{line[(size+1):]}</h{size}>' # This is the pattern of the header.
     return line
 
 
 def add_paragraph(line):
     # This verifies for any of the possible tags. If they aren't there, you add the paragraph marker.
-    m = re.match('<h|<ul|<p|<li', line)
-    if not m:
+    paragraph_match = re.match('<h|<ul|<p|<li', line)
+    if not paragraph_match:
         line = f"<p>{line}</p>"
     return line
 
 # These must be two functions because you CAN have both bold and italics. 
 def parse_bold(line):
-    m = re.match('(.*)__(.*)__(.*)', line) # Look for bold
-    if m:
-        line = f"{m.group(1)}<strong>{m.group(2)}</strong>{m.group(3)}" # parse bold
+    bold_match = re.match('(.*)__(.*)__(.*)', line) # Look for bold
+    if bold_match:
+        line = f"{bold_match.group(1)}<strong>{bold_match.group(2)}</strong>{bold_match.group(3)}" # parse bold
     return line
 
 
 def parse_italic(line):
-    m = re.match('(.*)_(.*)_(.*)', line) # Look for italics.
-    if m:
-        line = f"{m.group(1)}<em>{m.group(2)}</em>{m.group(3)}" # parse italics.
+    italic_match = re.match('(.*)_(.*)_(.*)', line) # Look for italics.
+    if italic_match:
+        line = f"{italic_match.group(1)}<em>{italic_match.group(2)}</em>{italic_match.group(3)}" # parse italics.
     return line 
 
 
@@ -41,9 +41,9 @@ def parse(markdown):
     in_list_append = False
     for i in lines:
         i = header_parsing(i)
-        m = re.match(r'\* (.*)', i) # Here we match for an *, which indicates a list
-        if m: # If you have a list
-            curr = m.group(1) # This gets whatever comes after the list signaling
+        list_match = re.match(r'\* (.*)', i) # Here we match for an *, which indicates a list
+        if list_match: # If you have a list
+            curr = list_match.group(1) # This gets whatever comes after the list signaling
             curr = parse_bold_italics(curr)
             i = f"<li>{curr}</li>" # Anything inside a list gets the list item tag.
             if not in_list:
